9960a3aa7fc8e75ed7a10bbbd6802a80553c5549
Alan Berndt <alan@eatabrick.org>
2012-12-06 12:18:33 -0700

    Get album art from last.fm

--- src/org/eatabrick/radio/NowPlayingFragment.java
+++ src/org/eatabrick/radio/NowPlayingFragment.java
@@ -1,6 +1,10 @@
 package org.eatabrick.radio;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
@@ -11,6 +15,19 @@ import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import com.actionbarsherlock.app.SherlockFragment;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import org.xml.sax.Attributes;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.DefaultHandler;
 
 public class NowPlayingFragment extends SherlockFragment {
   private static final String TAG = "NowPlayingFragment";
@@ -22,6 +39,96 @@ public class NowPlayingFragment extends SherlockFragment {
   private TextView  mElapsed;
   private TextView  mLength;
   private SeekBar   mSeek;
+
+  private class GetAlbumArtTask extends AsyncTask<String, Void, Void> {
+    protected Void doInBackground(String... strings) {
+      String artist = strings[0];
+      String album  = strings[1];
+
+      if (artist.equals("") || album.equals("")) return null;
+
+      DefaultHandler handler = new DefaultHandler() {
+        private boolean inElem = false;
+        private String  data = "";
+
+        public void startDocument() throws SAXException { }
+        public void endDocument() throws SAXException {
+          if (!data.equals("")) {
+            Log.d(TAG, "Album art found at " + data);
+            new LoadAlbumArtTask().execute(data);
+          } else {
+            Log.d(TAG, "No album art found");
+            new LoadAlbumArtTask().execute();
+          }
+        }
+
+        public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+          if (localName.equalsIgnoreCase("image")) {
+            String size = attributes.getValue("size");
+            if (size.equals("mega")) {
+              inElem = true;
+            }
+          }
+        }
+
+        public void endElement(String uri, String localName, String qName) throws SAXException {
+          if (inElem) inElem = false;
+        }
+
+        public void characters(char[] ch, int start, int length) throws SAXException {
+          if (inElem) {
+            data = data + new String(ch, start, length);
+          }
+        }
+      };
+
+      try {
+        Log.d(TAG, "Looking for art for " + artist + " - " + album);
+
+        Uri.Builder builder = Uri.parse("http://ws.audioscrobbler.com").buildUpon();
+        builder.path("/2.0/");
+        builder.appendQueryParameter("method", "album.getinfo");
+        builder.appendQueryParameter("api_key", "4827e70daf0106ae5a88b268c083e65b");
+        builder.appendQueryParameter("artist", artist);
+        builder.appendQueryParameter("album", album);
+
+        URL albumInfo = new URL(builder.build().toString());
+
+        XMLReader reader = SAXParserFactory.newInstance().newSAXParser().getXMLReader();
+        reader.setContentHandler(handler);
+        InputSource input = new InputSource(albumInfo.openStream());
+        reader.parse(input);
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+
+      return null;
+    }
+  }
+
+  private class LoadAlbumArtTask extends AsyncTask<String, Void, Bitmap> {
+    protected Bitmap doInBackground(String... strings) {
+      if (strings.length > 0) {
+        try {
+          URL url = new URL(strings[0]);
+          InputStream is = url.openConnection().getInputStream();
+          return BitmapFactory.decodeStream(is);
+        } catch (Exception e) {
+          Log.d(TAG, "Error: " + e.getMessage());
+        }
+      }
+
+      return null;
+    }
+
+    protected void onPostExecute(Bitmap result) {
+      if (result == null) {
+        mArt.setImageResource(R.drawable.album);
+      } else {
+        mArt.setImageBitmap(result);
+      }
+    }
+  }
 
   @Override public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
     View view = inflater.inflate(R.layout.playing, container, false);
@@ -44,11 +151,11 @@ public class NowPlayingFragment extends SherlockFragment {
   }
 
   public void updateSongInfo(String title, String artist, String album) {
-    // TODO get album art
-
     mTitle.setText(title);
     mArtist.setText(artist);
     mAlbum.setText(album);
+
+    new GetAlbumArtTask().execute(artist, album);
   }
 
   public void updateProgress(int elapsed, int length) {
